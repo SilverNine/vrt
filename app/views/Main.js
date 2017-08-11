582f4e6f3ad8ae8c0aa3764adfe8c23d373bba38
SilverNine <eungoo.jung@yellomobile.com>
2017-08-11 16:20:34 +0900

    dist ignore

--- app/views/Main.js
+++ app/views/Main.js
@@ -9,7 +9,7 @@ class Main extends Component {
                     <div className="col-lg-12">
                         <div className="text-center m-t-lg">
                             <h1>
-                                전정재활운동
+                                전정 재활
                             </h1>
                             <small>
                                 열심히 운동해서 같이 나아요!
